552ac36eb58504606c6445dd09cbd4c83ccb306c
Deepikapatel <deepikapatel5687@gmail.com>
2024-08-05 23:21:34 +0530

    anchor tag

---
+++ anchor tag/index.js
@@ -0,0 +1,11 @@
+//for add attribute
+let a= document.getElementById('anc');
+//target artibute href and link
+a.setAttribute('href','https://www.google.com')
+
+let b=document.getElementById('im');
+b.setAttribute('src','js.jpeg')
+
+//for remove attribute
+let c=document.getElementById('head');
+c.removeAttribute('style')
