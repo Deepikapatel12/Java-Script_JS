144011e47c14c5cf60f4523b0f99eb27eaf47813
Deepikapatel <deepikapatel5687@gmail.com>
2024-08-05 23:23:00 +0530

    array

---
+++ array/array.js
@@ -0,0 +1,37 @@
+//print all elements of single array
+// let array=[1,2,3,1.3,"a","b","cybrom"]
+// console.log(array)
+
+// //print only Q
+// let array1=[1,2,3,1.3,"a","b","cybrom"]
+// console.log(array1[2]) 
+
+// //
+// let arr2=[]
+//  for(let a=0;a<5;a++)
+//  {
+//     arr2[a]=prompt("enter no")
+//  }
+//  console.log(arr2)
+ 
+ //1to 10 element user input print cube of odd numbers
+ let arr3=[]
+ for(let i=0;i<=9;i++)
+ {      arr3[i]=prompt("enter no")
+arr3[i]*3
+   // if(arr3[i]/2!=0)
+   // {
+
+   //    arr3[i]*3
+   // }
+ }
+ console.log(arr3)
+
+
+ //short form of array function
+ //let c=(a+b)=>{a+b}
+ //let d=(a)=>(a*a)
+ //let d=a=>a*a
+ //without perameter it is compulsary to 
+ //let d=()=>
+  
