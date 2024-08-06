b7cbc6944660dfe59178849442207ffbe9e7813f
Deepikapatel <deepikapatel5687@gmail.com>
2024-08-07 01:03:45 +0530

    Array of object

---
+++ Array of object to store multiple data/arrob.js
@@ -0,0 +1,19 @@
+let emp =[
+    {
+        namee : "yawar",
+        age : 90,
+        city : "bpl",
+        contact : 25454524,
+        designation : "mazdoor"
+    },
+    {
+        namee : "sourabh",
+        age : 34,
+        city : "bpl",
+        contact :254651,
+        designation : "coach"
+    }
+]
+console.log(emp);
+console.log(emp[0]);
+console.log(emp[1]);
