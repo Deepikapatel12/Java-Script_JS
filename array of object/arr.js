0a396fa7160770b62e3cfceab4af9d4a4be2d8ed
Deepikapatel <deepikapatel5687@gmail.com>
2024-08-06 22:49:37 +0530

    array of object

---
+++ array of object/arr.js
@@ -0,0 +1,33 @@
+let product =[
+    {
+        namee:"hp",
+        generation: "11th",
+        series:"home",
+    },
+    {
+        namee:"dell",
+        generation: "7th",
+        series:"corporate",
+    },
+    {
+        namee:"lenvo",
+        generation: "8th",
+        series:"office",
+    }
+]
+let nm={
+    namee:"asus",
+    generation:"6th",
+    series:"gaming"
+}
+let sh={
+    nam:"dd",
+
+}
+console.log(product)
+product.push(nm)
+console.log(product)
+product.pop(nm)
+console.log(product)
+product.shift(sh)
+console.log(product)
