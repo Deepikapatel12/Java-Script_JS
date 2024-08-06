ed4639a824a4c4566971c99c8d6ac56eee5fcf9f
Deepikapatel <deepikapatel5687@gmail.com>
2024-08-07 01:05:44 +0530

    Arrow Function

---
+++ Arrow Function/arrow.js
@@ -0,0 +1,36 @@
+//syntax= () => {return value}
+//Assign to the variable
+
+let fun = () => {return 9}
+ console.log(fun());
+
+
+
+ let a=10
+ let fun1 =(a) =>{return a+a}
+ console.log(fun1(a))
+
+ let r=parseInt(prompt("enter the radius"))
+ let fun2 =(ar) =>{return 3.14*r*r}
+ console.log(fun2(r));
+
+ //write a prog. to find simple interst
+ let p=parseInt(prompt("emter ammount"))
+ let t=parseInt(prompt("enter time"))
+ let ra=parseInt(prompt("enter intrest"))
+ let to=(p*r*t)/100;
+ let fun3=(to2) =>{return (p*r*t)/100}
+ console.log(fun3(to));
+ 
+ let even=parseInt(prompt("enter any number"))
+ let fun4=(even) => {return 
+    if(even%2==0)
+        {
+            console.log("even")
+        }
+        else{
+            console.log("odd")
+        }
+ }
+ console.log(fun4(even));
+
