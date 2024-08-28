f18dc162bc2626510b59f7231b3e43b02eabe70d
Deepikapatel <deepikapatel5687@gmail.com>
2024-08-28 22:30:00 +0530

    j query

---
+++ j query/query.js
@@ -0,0 +1,13 @@
+//left side selector right side function
+// . is a access member operator
+//jb document ready hoga jb function bega
+//selector bnyge to humesha $ ka sign lgaga
+//as an argument function pass kiya h
+$(document).ready(function(){
+    let a= $('#head').html()
+    console.log(a);
+
+    $('#btn').click(function(){
+        alert("Clicked..!!!!!")
+    })
+})
