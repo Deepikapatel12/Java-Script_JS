a2e60435c2a22bd73a1d6674b6fe1cb42230479c
Deepikapatel <deepikapatel5687@gmail.com>
2024-08-31 21:53:25 +0530

    jquery

---
+++ jquery2/j.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    let a=$('.head').html()
+    console.log(a);
+
+   let b= $('#pera').html()
+console.log(b);
+let c =$('button').click()
+
+})
+
+
+
+
+// $(document).ready(function(){
+//     let a= $('#head').html()
+//     console.log(a);
+
+//     $('#btn').click(function(){
+//         alert("Clicked..!!!!!")
+//     })
+// })
