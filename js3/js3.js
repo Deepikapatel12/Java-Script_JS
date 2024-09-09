84ec9dab2b4ec083fe923a96347887104796ef74
Deepikapatel <deepikapatel5687@gmail.com>
2024-09-09 23:09:04 +0530

    js3

---
+++ js3/js3.js
@@ -0,0 +1,157 @@
+// let a =90
+// let b=90
+// console.log(a+b)
+//operators unary binary ternary
+/*  
+1 unary=++, --
+2 binary=arthmatic + - * / %
+relational= < > <= >= != == ===
+assignment operators += = -= *= /=
+logical operators && || !
+
+3 Turnary operator 
+? :
+*/
+
+//   let a= parseInt( prompt("enter the value"))
+//   let b= parseInt( prompt("enter the value"))
+//   console.log(a+b)
+
+ //area of circle A=3.14*r*r
+//   let radius=parseFloat(prompt("enter radius"))
+//   let p=3.14
+//   console.log(circle=p*radius*radius)
+ 
+ //area of rectangle A=l*w
+//  let l=parseFloat(prompt("enterlength"))
+//  let w=parseFloat(prompt("enter width"))
+//  console.log(rectangle=l*w)
+
+//area of triangle A=b*h/2
+//  let h=parseFloat(prompt("enter height"))
+//  let bi=parseFloat(prompt("enter bidth"))
+//  console.log(triangle=1/2*h*bi)
+
+//swap two values
+// let c=parseInt(prompt("enter 1st no"))
+// let d=parseInt(prompt("enter 2nd no"))
+// let e
+// e=c
+// c=d
+// d=e
+// console.log(c,d)
+
+
+// //single condition
+// //if else statement
+// let n1=parseInt(prompt("enter no"))
+// if(n1>0)
+// {
+//     console.log("positive")
+// }
+// else{
+//     console.log("negative")
+// }
+
+// //divisible by 7 or not
+// let n2=parseInt(prompt("enter no"))
+// if(n2%7==0)
+// {
+//     console.log("divisible")
+// }
+// else{
+//     console.log("not divisible")
+// }
+
+
+
+
+
+
+//for multiple condition use if else ladder
+// let f1=parseInt(prompt("enter your marks"))
+
+// if(f1>=70 && f1>=100)
+// {
+//     console.log("first division")
+// }
+// else if(f1>=41 && f1>=70)
+// {
+//     console.log("2nd division")
+// }
+// else if(f1>=10 && f1>=40)
+// {
+//     console.log("fail")
+// }
+// else
+// {
+//     console.log("wrong input")
+// }
+
+
+//nested if else
+
+// let age=parseInt(prompt("enter your age"))
+// if(age>=18)
+// {
+//     let i=parseInt(prompt("press 1 for indian"))
+//     if(i==1)
+//      {
+//        alert("eligible for voting bcz ur indian")
+//     }
+//     else{
+//         alert("not eligible bcz ur not indian ")
+//     }
+// }
+// else{
+//     alert("not eligible bcz ur child")
+// }
+
+
+ //switch statement
+
+//  let day=parseInt(prompt("enter any no"))
+//  switch(day)
+//  {
+//     case 1:
+//     alert("monday")
+//     break;
+//     case 2:
+//     alert("tuesday")
+//     break;
+//     case 3:
+//     alert("wednesday")
+//     break;
+//     case 4:
+//         alert("thursday")
+//         break;
+//         case 5:
+//             alert("friday")
+//             break;
+//             case 6:
+//                 alert("saturday")
+//                 break;
+//                 case 7:
+//                     alert("sunday")
+//                     break;
+
+//                     default:
+//                    alert("wrong input")
+//  }
+ 
+
+ let value=parseInt(prompt("enter no"))
+ switch(value)
+ {
+    case 1:
+        alert(4*4)
+        break;
+        case 2:
+            alert(8*8)
+            break;
+            case 3:
+                alert(3*3)
+                break;
+                default:
+                alert("wrong input")
+ }
